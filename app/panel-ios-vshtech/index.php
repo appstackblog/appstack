cdd8f5ccdac20810a98e1b5209584ac3625a4a11
appstackblog <hdangchu06@gmail.com>
2025-12-13 10:27:35 +0700

    Add files via upload

--- app/panel-ios-vshtech/index.php
+++ app/panel-ios-vshtech/index.php
@@ -1,3 +1,22 @@
 <?php
-// Serve the PANEL IOS page
-readfile(__DIR__ . '/index.html');
+require __DIR__ . '/time.php';
+
+$cssVer = asset_ver('lumina.css');
+$jsVer  = asset_ver('lumina.js');
+$panelVer = asset_ver('../assets/icons/panel.png');
+
+$html = file_get_contents(__DIR__ . '/index.html');
+$search = [
+  'lumina.css',
+  'lumina.js',
+  '../assets/icons/panel.png',
+  '../index.html'
+];
+$replace = [
+  'lumina.css?v=' . $cssVer,
+  'lumina.js?v=' . $jsVer,
+  '../assets/icons/panel.png?v=' . $panelVer,
+  '../index.php'
+];
+
+echo str_replace($search, $replace, $html);
