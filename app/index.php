e461b627b8ac575bdda6142d142e265d26e02062
appstackblog <hdangchu06@gmail.com>
2025-12-13 10:27:07 +0700

    Add files via upload

--- app/index.php
+++ app/index.php
@@ -1,4 +1,31 @@
 <?php
-// Temporary maintenance redirect to static notice
-header('Content-Type: text/html; charset=UTF-8');
-readfile(__DIR__ . '/index.html');
+require __DIR__ . '/time.php';
+
+$cssVer = asset_ver('mainstyle.css');
+$jsVer  = asset_ver('mainscript.js');
+$avatarVer = asset_ver('assets/avatar.png');
+$panelIconVer = asset_ver('assets/icons/panel.png');
+$fatIconVer = asset_ver('assets/icons/fat.png');
+$dnsIconVer = asset_ver('assets/dns.png');
+
+$html = file_get_contents(__DIR__ . '/index.html');
+
+$search = [
+  'mainstyle.css',
+  'mainscript.js',
+  'assets/avatar.png',
+  'assets/icons/panel.png',
+  'assets/icons/fat.png',
+  'assets/dns.png'
+];
+
+$replace = [
+  'mainstyle.css?v=' . $cssVer,
+  'mainscript.js?v=' . $jsVer,
+  'assets/avatar.png?v=' . $avatarVer,
+  'assets/icons/panel.png?v=' . $panelIconVer,
+  'assets/icons/fat.png?v=' . $fatIconVer,
+  'assets/dns.png?v=' . $dnsIconVer
+];
+
+echo str_replace($search, $replace, $html);
