d4d78d7cf8b5a263ad2e7147140726577d944690
appstackblog <hdangchu06@gmail.com>
2025-12-13 10:45:03 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ if (is_file($main)) {
 $cssVer  = asset_ver('quasar-veil.css');
 $jsVer   = asset_ver('orbit-wisp.js');
 $logoVer = asset_ver('img/hehe.gif');
+$searchIconVer = asset_ver('img/kinhlup.svg');
 
 $html = file_get_contents(__DIR__ . '/index.html');
 
@@ -26,4 +27,13 @@ $replace = [
   'img/hehe.gif?v=' . $logoVer
 ];
 
-echo str_replace($search, $replace, $html);
+$html = str_replace($search, $replace, $html);
+
+// Override search icon with versioned SVG
+$html = str_replace(
+  '</head>',
+  "<style>.search-icon{background:url(\"img/kinhlup.svg?v={$searchIconVer}\") center/contain no-repeat;}</style></head>",
+  $html
+);
+
+echo $html;
