359870203493fe24995c2a7a03df9b52df487562
appstackblog <hdangchu06@gmail.com>
2025-12-16 21:01:33 +0700

    Add files via upload

--- app/fatx007-ios-turbo/index.php
+++ app/fatx007-ios-turbo/index.php
@@ -1,3 +1,33 @@
 <?php
-// Serve the Fatx007 Turbo page
-readfile(__DIR__ . '/index.html');
+require __DIR__ . '/time.php';
+
+$cssVer   = asset_ver('falcon.css');
+$jsVer    = asset_ver('falcon.js');
+$fatVer   = asset_ver('../assets/icons/fat.png');
+$panelVer = asset_ver('../assets/icons/panel.png');
+$dnsVer   = asset_ver('../assets/dns.png');
+$avatarVer = asset_ver('../assets/avatar.png');
+
+$html = file_get_contents(__DIR__ . '/index.html');
+
+$search = [
+  'falcon.css',
+  'falcon.js',
+  '../assets/icons/fat.png',
+  '../assets/icons/panel.png',
+  '../assets/dns.png',
+  '../index.html',
+  '../assets/avatar.png',
+];
+
+$replace = [
+  'falcon.css?v=' . $cssVer,
+  'falcon.js?v=' . $jsVer,
+  '../assets/icons/fat.png?v=' . $fatVer,
+  '../assets/icons/panel.png?v=' . $panelVer,
+  '../assets/dns.png?v=' . $dnsVer,
+  '../index.php',
+  '../assets/avatar.png?v=' . $avatarVer,
+];
+
+echo str_replace($search, $replace, $html);
