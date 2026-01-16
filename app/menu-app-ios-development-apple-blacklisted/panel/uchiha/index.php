bc714829b6351d1447fb5ad81b745629a578a60c
appstackblog <hdangchu06@gmail.com>
2026-01-16 20:28:52 +0700

    Create index.php

---
+++ app/menu-app-ios-development-apple-blacklisted/panel/uchiha/index.php
@@ -0,0 +1,28 @@
+<?php
+require __DIR__ . '/time.php';
+
+$htmlPath = __DIR__ . '/index.html';
+if (!is_file($htmlPath)) {
+  http_response_code(404);
+  exit('index.html not found');
+}
+
+$cssVer = asset_ver('styles.css');
+$appVer = asset_ver('app.js');
+$keyVer = asset_ver('key.js');
+
+$html = file_get_contents($htmlPath);
+
+$search = [
+  'styles.css',
+  'app.js',
+  'key.js',
+];
+
+$replace = [
+  "styles.css?v={$cssVer}",
+  "app.js?v={$appVer}",
+  "key.js?v={$keyVer}",
+];
+
+echo str_replace($search, $replace, $html);
