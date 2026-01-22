f23c64c9d9fe47f31aa6cf8a2560e1b6fb1f2391
appstackblog <hdangchu06@gmail.com>
2026-01-22 16:36:10 +0700

    Delete app/menu-app-ios-development-apple-blacklisted/panel/panel-ios-vshtech directory

--- app/menu-app-ios-development-apple-blacklisted/panel/panel-ios-vshtech/code/app-loader.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Content-Type: application/javascript; charset=UTF-8');
-readfile(__DIR__ . '/app-long-protected.js');
--- app/menu-app-ios-development-apple-blacklisted/panel/panel-ios-vshtech/time.php
+++
@@ -1,8 +0,0 @@
-<?php
-function asset_ver(string $relativePath): string {
-  $path = __DIR__ . '/' . ltrim($relativePath, '/');
-  if (file_exists($path)) {
-    return (string)filemtime($path);
-  }
-  return (string)time();
-}
