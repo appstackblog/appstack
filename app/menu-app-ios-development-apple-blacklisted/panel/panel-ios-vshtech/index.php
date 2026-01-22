c97307ac17b385f517a8c2d7a69ddc3c3e3bd0b5
appstackblog <hdangchu06@gmail.com>
2026-01-22 17:31:02 +0700

    Add files via upload

--- app/menu-app-ios-development-apple-blacklisted/panel/panel-ios-vshtech/index.php
+++ app/menu-app-ios-development-apple-blacklisted/panel/panel-ios-vshtech/index.php
@@ -9,35 +9,47 @@ if (!is_file($htmlPath)) {
 
 $cssVer = asset_ver('color/theme.css');
 $jsVer  = asset_ver('keygate.js');
-$javaFiles = [
-  'aimlock.js',
-  'anchorAim.js',
-  'app.js',
-  'driftFix.js',
-  'featherAim.js',
-  'headFix.js',
-  'quickSwipe.js',
-  'screenBoost.js',
-  'shakeFix.js',
-  'steadyHold.js',
-];
+$aimlockVer = asset_ver('java/aimlock.js');
+$anchorAimVer = asset_ver('java/anchorAim.js');
+$appVer = asset_ver('java/app.js');
+$driftFixVer = asset_ver('java/driftFix.js');
+$featherAimVer = asset_ver('java/featherAim.js');
+$headFixVer = asset_ver('java/headFix.js');
+$quickSwipeVer = asset_ver('java/quickSwipe.js');
+$screenBoostVer = asset_ver('java/screenBoost.js');
+$shakeFixVer = asset_ver('java/shakeFix.js');
+$steadyHoldVer = asset_ver('java/steadyHold.js');
 
 $html = file_get_contents($htmlPath);
 
 $search = [
   './color/theme.css',
   './keygate.js',
+  './java/aimlock.js',
+  './java/anchorAim.js',
+  './java/app.js',
+  './java/driftFix.js',
+  './java/featherAim.js',
+  './java/headFix.js',
+  './java/quickSwipe.js',
+  './java/screenBoost.js',
+  './java/shakeFix.js',
+  './java/steadyHold.js',
 ];
 
 $replace = [
   "./color/theme.css?v={$cssVer}",
   "./keygate.js?v={$jsVer}",
+  "./java/aimlock.js?v={$aimlockVer}",
+  "./java/anchorAim.js?v={$anchorAimVer}",
+  "./java/app.js?v={$appVer}",
+  "./java/driftFix.js?v={$driftFixVer}",
+  "./java/featherAim.js?v={$featherAimVer}",
+  "./java/headFix.js?v={$headFixVer}",
+  "./java/quickSwipe.js?v={$quickSwipeVer}",
+  "./java/screenBoost.js?v={$screenBoostVer}",
+  "./java/shakeFix.js?v={$shakeFixVer}",
+  "./java/steadyHold.js?v={$steadyHoldVer}",
 ];
-
-foreach ($javaFiles as $file) {
-  $ver = asset_ver("java/{$file}");
-  $search[] = "./java/{$file}";
-  $replace[] = "./java/{$file}?v={$ver}";
-}
 
 echo str_replace($search, $replace, $html);
